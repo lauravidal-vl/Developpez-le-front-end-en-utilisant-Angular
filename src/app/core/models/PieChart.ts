6fd05b82882f94edf94cec0b6864f2040bbc2bd0
laura vidal <lauravidal.vl@gmail.com>
2024-12-15 19:22:42 +0100

    ajout du routing de la page principal à la page détail depuis le graphique'

---
+++ src/app/core/models/PieChart.ts
@@ -0,0 +1,5 @@
+export interface PieChart {
+  id: number;
+  name: string;
+  value: number;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { PieChart } from 'src/app/core/models/PieChart';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,11 +11,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  @Input() olympic!: Olympic;
   // public olympics$: Observable<any> = of(null);
   public olympics$: Observable<Olympic[]> = of([]);
-  public pieChartData: any[] = [];
+  public pieChartData!: PieChart[];
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(private olympicService: OlympicService,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
@@ -28,11 +32,29 @@ export class HomeComponent implements OnInit {
 
   updatePieChartData(): void {
     this.olympics$.subscribe((olympics) => {
-      this.pieChartData = olympics.map((olympic) => ({
-        name: olympic.country,
-        value: this.getTotalMedals(olympic),
-      }));
+      this.pieChartData = olympics.map((olympic) => {
+        return {
+          id: olympic.id,
+          name: olympic.country,
+          value: this.getTotalMedals(olympic),
+        };
+      });
     });
   }
+
+      // Méthode appelée lors du clic sur un segment du graphique
+    onSelect(event: any): void {
+      const selectedCountryName = event.name;
+      // Trouver l'ID correspondant au pays sélectionné
+      const selectedCountry = this.pieChartData.find(
+        (country) => country.name === selectedCountryName
+      );
+      if (selectedCountry) {
+        console.log('ID:', selectedCountry.id);  // Vous pouvez maintenant accéder à l'ID
+        this.router.navigate([`${selectedCountry.id}`]);  // Naviguer vers la page de détail
+      } else {
+        console.log('Country not found!');
+      }
+    }
 
 }
