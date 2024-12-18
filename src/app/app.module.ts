314871842c37bd48fd3f4991d359d9551b8bc007
laura vidal <lauravidal.vl@gmail.com>
2024-12-18 20:47:27 +0100

    Désactivation des animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -10,7 +11,7 @@ import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent, DetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule, NoopAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
